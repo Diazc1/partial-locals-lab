919a2796f6f5fc45e4e3f4eba681a91efe2bee7b
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-21 11:47:45 -0700

    commit tree 931fd0c39e2dafabfc8973670dd2fe6597189f50

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,15 @@
 class ClassroomsController < ApplicationController
+
   def show
     @classroom = Classroom.find(params[:id])
-    @student = @classroom.oldest_student
   end
 
-  def index
-    @classrooms = Classroom.all
-  end
+#   def show
+#     @classroom = Classroom.find(params[:id])
+#     @student = @classroom.oldest_student
+#   end
+
+  # def index
+  #   @classrooms = Classroom.all
+  # end
 end
